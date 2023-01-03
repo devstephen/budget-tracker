75424b3dc5a68307039d03282efbbfeb62480fa5
Fatolu Stephen <smjssp@gmail.com>
2023-01-03 08:12:17 +0100

    Set componnent level state in AddTransaction.js

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
   return (
     <div>
       <h3>Add new transaction</h3>
       <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" placeholder="Enter description..." />
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter description..."
+          />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" placeholder="Enter amount..." />
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
         </div>
         <button className="btn">Add transaction</button>
       </form>
