ebe2f37af5b4f315053059f930ffa781f4d31322
Fatolu Stephen <smjssp@gmail.com>
2023-01-03 09:22:28 +0100

    Fix transaction component

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 export const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
   return (
-    <li className="minus">
-      {transaction.text} <span>$400</span>
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+      {transaction.text}{" "}
+      <span>
+        {sign}
+        {Math.abs(transaction.amount)}
+      </span>
       {new Date().toLocaleDateString()}
       <button className="delete-btn">❌</button>
     </li>
